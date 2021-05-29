1b9fabc2bd98df75c329828a1199a4ddafb92bcb
iamnehal <iamnehalchandra@gmail.com>
2021-05-29 23:28:47 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Header  from "./mycompoents/Header";
+import Footer from "./mycompoents/Footer";
+import Todos from "./mycompoents/Todos";
+
+
+
+function App() {
+  return (
+ <>
+    
+<Header/>
+<Footer/>
+<Todos/>
+
+
+
+ </>
+  );
+}
+
+export default App;
